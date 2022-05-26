7cdccc22133960871bb121bd808153e802e459b3
Hin Lam <hinl@C02Z2216LVDL.vmware.com>
2022-05-27 00:06:15 +0800

    Update install.sh for new tanzu cli installation

--- tanzu-harbor-plugin/install.sh
+++ tanzu-harbor-plugin/install.sh
@@ -2,6 +2,8 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 TANZU_PLUGIN_PUBLISH_PATH=$HOME/.config/tanzu-plugins
 
+mkdir -p $TANZU_PLUGIN_PUBLISH_PATH/distribution/
+mkdir -p $TANZU_PLUGIN_PUBLISH_PATH/discovery/
 cp -rvf $SCRIPT_DIR/distribution/* $TANZU_PLUGIN_PUBLISH_PATH/distribution/
 cp -rvf $SCRIPT_DIR/discovery/* $TANZU_PLUGIN_PUBLISH_PATH/discovery/
 tanzu plugin source add --name harbor-local-source --type local --uri $TANZU_PLUGIN_PUBLISH_PATH/discovery/harbor
